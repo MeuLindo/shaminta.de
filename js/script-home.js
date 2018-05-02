8269f719e9f5c3efdf89843063d565dc78622269
Alexandre Faria <alexandreffaria@me.com>
2018-05-02 14:47:33 -0300

    Push inicial

---
+++ js/script-home.js
@@ -0,0 +1,9 @@
+// $( document ).ready(function(){
+//       var userLang = navigator.language || navigator.userLanguage;
+//       if (userLang == "pt-BR") {
+//           window.location.href = "deutsch/register.html"
+//       }
+//       else {
+//
+//       }
+//   });
